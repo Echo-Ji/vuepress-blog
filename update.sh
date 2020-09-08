d69cb61fb0999b8d9d1f8cb7a1c1ac673b781a10
“you <bh1506jjh@buaa.edu.cn>
2020-09-08 22:28:36 +0800

    ::[Drocket: 20200908 cluster update, bash update

--- update.sh
+++ update.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
 
+read -p "What is your update comment: " comment
+# echo "$comment"
+
+# 运行脚本时打印出来
+set -v 
+
 git add -A
-git commit -m "$1"
+git commit -m "$comment"
 git push
